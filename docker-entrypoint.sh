78e67a002de31240910c724c68f01abc5dae59cd
clde-ber <claire.deberny@gmail.com>
2020-11-02 10:28:48 +0100

    error est database connexion fixed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,9 @@
-mysqld --pid-file=/var/run/mysqld/mysqld.pid --user=root -D && \
-echo "CREATE DATABASE wordpress;" | mysql --port=13306 --host=localhost --user=root && \
-echo "USE wordpress;" | mysql --port=13306 --host=localhost --user=root && \
-echo "GRANT ALL ON wordpress.* TO 'root'@'localhost';" | mysql --port=13306 --host=localhost --user=root && \
-echo "FLUSH PRIVILEGES;" | mysql --port=13306 --host=localhost --user=root && echo "EXIT;" | mysql --port=13306 --host=localhost --user=root && \
-echo "connect wordpress;" | mysql --port=13306 --host=localhost --user=root
+mysqld --pid-file=/var/run/mysqld/mysqld.pid --user=root -D
+echo "CREATE DATABASE wordpress;" | mysql --port=13306 --host=localhost --user=root
+echo "USE wordpress;" | mysql --port=13306 --host=localhost --user=root
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" | mysql --port=13306 --host=localhost --user=root
+echo "update mysql.user set plugin='mysql_native_password' where user='root';" | mysql --port=13306 --host=localhost --user=root
+echo "FLUSH PRIVILEGES;" | mysql --port=13306 --host=localhost --user=root
 service nginx start
 nginx -g daemon off;
 service php7.3-fpm start
